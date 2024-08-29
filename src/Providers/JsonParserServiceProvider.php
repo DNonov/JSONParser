c9309d284aa8beec8965afbc27dbc0348031c87b
Dimitar Nonov <d.nonov@gmail.com>
2024-08-29 22:50:37 +0300

    Remove dd in boot

--- src/Providers/JsonParserServiceProvider.php
+++ src/Providers/JsonParserServiceProvider.php
@@ -5,6 +5,6 @@ namespace Dnonov\JsonParser\Providers;
 use Illuminate\Support\ServiceProvider;
 
 class JsonParserServiceProvider extends ServiceProvider {
-    public function boot(): void { dd('v'); }
+    public function boot(): void {}
     public function register(): void {}
 }
